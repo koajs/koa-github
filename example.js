26299bd39e87e31194d4f64e21f8f603ff7803ec
dead_horse <dead_horse@qq.com>
2014-01-01 03:11:17 +0800

    add example

---
+++ example.js
@@ -0,0 +1,36 @@
+//use http://localhost:7001 to test
+
+var koa = require('koa');
+var http = require('http');
+var session = require('koa-sess');
+var githubAuth = require('./');
+
+var app = koa();
+
+app.name = 'nae-web';
+app.keys = ['key1', 'key2'];
+
+app.use(session());
+app.use(githubAuth({
+  clientID: '5ec1d25d2a3baf99a03c',
+  clientSecret: '513607494a244e2759738cae3d50a89494c1e7f0',
+  callbackURL: 'http://localhost:7001',
+  userKey: 'user',
+  timeout: 10000
+}));
+
+app.use(function *handler() {
+  if (!this.session.githubToken) {
+    this.body = '<a href="/github/auth">login with github</a>';
+  } else {
+    this.body = this.session.user;
+  }
+});
+
+app.on('error', function (err) {
+  if (!err.status || err.status >= 500) {
+    logger.error(err);
+  }
+});
+
+http.createServer(app.callback()).listen(7001);
